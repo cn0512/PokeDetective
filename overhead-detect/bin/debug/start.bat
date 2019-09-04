a15eeb80a46ce45e5764c110b69cae64b972291f
mp <cn0512@126.com>
2019-09-04 15:47:51 +0800

    Signed-off-by: mp <cn0512@126.com>

--- overhead-detect/bin/debug/start.bat
+++ overhead-detect/bin/debug/start.bat
@@ -1,4 +1,7 @@
 REM people-counter.exe 1 1 180 540 bus.avi
-people-counter.exe 1 1 120 150 bus.avi
+REM people-counter.exe 1 1 120 150 bus.avi
+REM people-counter.exe 1 1 120 150 Vid_01.mp4
+REM people-counter.exe 1 1 120 820 1.avi
+people-counter.exe 0 1 120 600 1.avi
 
 pause
