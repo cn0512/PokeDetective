70d7a76c309b4ccd82434ef053c47f42d62e3a4b
mp <cn0512@126.com>
2019-09-04 17:44:37 +0800

    Signed-off-by: mp <cn0512@126.com>

--- overhead-detect/bin/debug/start.bat
+++ overhead-detect/bin/debug/start.bat
@@ -2,6 +2,11 @@ REM people-counter.exe 1 1 180 540 bus.avi
 REM people-counter.exe 1 1 120 150 bus.avi
 REM people-counter.exe 1 1 120 150 Vid_01.mp4
 REM people-counter.exe 1 1 120 820 1.avi
-people-counter.exe 0 1 120 600 1.avi
+REM people-counter.exe 0 0 120 320 1.avi
+REM people-counter.exe 1 1 120 600 w1.mp4
+REM people-counter.exe 1 1 120 600 w2.mp4
+REM people-counter.exe 1 1 120 720 w3.mp4
+REM people-counter.exe 1 1 120 600 w4.mp4
+people-counter.exe 1 1 120 720 w5.mp4
 
 pause
--- overhead-detect/src/modules/counter/VideoCapturePeopleCounter.cpp
+++ overhead-detect/src/modules/counter/VideoCapturePeopleCounter.cpp
@@ -59,13 +59,62 @@ public:
     }
 
     // member methods
+	void GetGammaTransLUT(uchar *pLUT, float Gamma, int iLUTLen)
+	{
+		for (int i = 0; i < iLUTLen; i++)
+		{
+			pLUT[i] = (uchar)(pow((float)i / 255.0, Gamma) * 255);
+		}
+	}
+	void GammaTrans(uchar *pSrc, uchar *pDst, const int iHeight,
+		const int iWidth, float Gamma)
+	{
+		uchar *pLUT = new uchar[256];
+		GetGammaTransLUT(pLUT, Gamma, 256);
+		for (int i = 0; i < iHeight*iWidth; i++)
+		{
+			pDst[i] = (uchar)pLUT[pSrc[i]];
+		}
+		delete[]pLUT;
+	}
+
+
 
     void start() {
         Mat frame;
 		if (m_type == 1) {
 			VideoCapture videoCapture(videoCapturePath);
+			//videoCapture.set(CV_CAP_PROP_FPS, 30);//帧数
+			//videoCapture.set(CV_CAP_PROP_BRIGHTNESS, 1);//亮度 1
+			//videoCapture.set(CV_CAP_PROP_CONTRAST, 10);//对比度 40
+			//videoCapture.set(CV_CAP_PROP_SATURATION, 50);//饱和度 50
+			//videoCapture.set(CV_CAP_PROP_HUE, 50);//色调 50
+			//videoCapture.set(CV_CAP_PROP_EXPOSURE, 0);//曝光 50
+
+			//滤波器的核
+			int kernel_size = 3;
+			Mat kernel = Mat::ones(kernel_size, kernel_size, CV_32F) / (float)(kernel_size*kernel_size);
+		
 			while (videoCapture.isOpened()) {
 				if (!videoCapture.read(frame)) break;
+
+				//-------------------------------------------------------------
+				//灰度
+				cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+
+				//gama
+				//Mat frame2(frame.size(), frame.type());				
+				uchar* pSrc = frame.data;
+				uchar* pDst = frame.data;
+				const int iHeight = frame.rows;
+				const int iWidth = frame.cols;
+				GammaTrans(pSrc, pDst, iHeight, iWidth, 0.3);
+
+				//滤波
+				filter2D(frame, frame, -1, kernel);
+				GaussianBlur(frame, frame, Size(3, 3), 0, 0);
+				//-------------------------------------------------------------
+
 				if (++frameNumber == 1) {
 					refLine = Line(0, refLineY, frame.cols, refLineY);
 					refLine2 = Line(0, refLineY2, frame.cols, refLineY2);
@@ -85,12 +134,12 @@ public:
 		videoCapture.open(m_nDeviceIdx);
 		videoCapture.set(CV_CAP_PROP_FRAME_WIDTH, S_W);//宽度
 		videoCapture.set(CV_CAP_PROP_FRAME_HEIGHT, S_H);//高度
-		//videoCapture.set(CV_CAP_PROP_FPS, 30);//帧数
-		//videoCapture.set(CV_CAP_PROP_BRIGHTNESS, 1);//亮度 1
-		//videoCapture.set(CV_CAP_PROP_CONTRAST, 40);//对比度 40
-		//videoCapture.set(CV_CAP_PROP_SATURATION, 50);//饱和度 50
-		//videoCapture.set(CV_CAP_PROP_HUE, 50);//色调 50
-		//videoCapture.set(CV_CAP_PROP_EXPOSURE, 50);//曝光 50
+		videoCapture.set(CV_CAP_PROP_FPS, 30);//帧数
+		videoCapture.set(CV_CAP_PROP_BRIGHTNESS, 1);//亮度 1
+		videoCapture.set(CV_CAP_PROP_CONTRAST, 40);//对比度 40
+		videoCapture.set(CV_CAP_PROP_SATURATION, 50);//饱和度 50
+		videoCapture.set(CV_CAP_PROP_HUE, 50);//色调 50
+		videoCapture.set(CV_CAP_PROP_EXPOSURE, 50);//曝光 50
 
         while (videoCapture.isOpened()) {
             if (!videoCapture.read(frame)) break;
@@ -207,7 +256,7 @@ private:
 
 	bool isPersonCrossingTheRefLine2(const Person* person, Line line, Line line2, int* direction = NULL) {
 		for (int i = 0; i < linesCrossedByPerson[person].size(); i++) {
-			if (line == linesCrossedByPerson[person][i]) {
+			if (line == linesCrossedByPerson[person][i] || line2 == linesCrossedByPerson[person][i]) {
 				return false;
 			}
 		}
@@ -222,6 +271,15 @@ private:
 					linesCrossedByPerson[person].push_back(line);
 					return true;
 				}
+				if (intersect(person->trace[i], person->trace[i + 1], line2.start, line2.end)) {
+					if (direction != NULL) {
+						*direction = person->trace[i].y > line.start.y && person->trace[i].y < line2.start.y ? LINE_DIRECTION_UP : LINE_DIRECTION_DOWN;
+					}
+
+					linesCrossedByPerson[person].push_back(line);
+					return true;
+				}
+
 			}
 		}
 
